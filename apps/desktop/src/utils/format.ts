b781b8fa46ff9c5bfeb7a7d0885b16744f32f8ac
ItsLhuis <itslhuis.dev@gmail.com>
2025-04-24 18:19:26 +0100

    Add internationalization to return the date according to the selected language

--- apps/desktop/src/utils/format.ts
+++ apps/desktop/src/utils/format.ts
@@ -1,17 +1,25 @@
-import { differenceInHours, format, isToday, isYesterday, parseISO } from "date-fns"
+import { differenceInHours, isToday, isYesterday, parseISO } from "date-fns"
+
+import i18n from "@i18n/config"
 
 export function formatRelativeDate(data: Date | string): string {
-  const date = typeof data === "string" ? parseISO(data) : data
+  const t = i18n.t
+  const lang = i18n.language
 
+  const date = typeof data === "string" ? parseISO(data) : data
   const diffHoras = differenceInHours(new Date(), date)
 
   if (diffHoras < 24) {
-    if (diffHoras < 1) return "Less than an hour ago"
-    return `${diffHoras} ${diffHoras === 1 ? "hour" : "hours"} ago`
+    if (diffHoras < 1) return t("common.lessThanAnHourAgo")
+    return t("common.hoursAgo", { count: diffHoras })
   }
 
-  if (isToday(date)) return "Today"
-  if (isYesterday(date)) return "Yesterday"
+  if (isToday(date)) return t("common.today")
+  if (isYesterday(date)) return t("common.yesterday")
 
-  return format(date, "d MMMM, yyyy")
+  return new Intl.DateTimeFormat(lang, {
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+  }).format(date)
 }
