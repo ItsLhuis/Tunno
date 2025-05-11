56b1ace3fbf11b141430be878aff6b007076a9a9
ItsLhuis <itslhuis.dev@gmail.com>
2025-05-11 20:51:12 +0100

    Update shadcnui script

--- apps/desktop/tailwind.config.ts
+++ apps/desktop/tailwind.config.ts
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import { type Config } from "tailwindcss"
+
 export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
@@ -123,4 +124,4 @@ export default {
     }
   },
   plugins: [require("tailwindcss-animate")]
-}
+} satisfies Config
