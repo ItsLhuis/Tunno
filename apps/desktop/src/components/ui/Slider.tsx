c2636ab337bab77d004260798f76f7622179c2ad
ItsLhuis <itslhuis.dev@gmail.com>
2025-10-03 16:21:38 +0100

    Enhance Slider component with vertical orientation support

--- apps/desktop/src/components/ui/Slider.tsx
+++ apps/desktop/src/components/ui/Slider.tsx
@@ -1,6 +1,12 @@
 "use client"
 
-import { forwardRef, useState, type ComponentPropsWithoutRef, type ElementRef } from "react"
+import {
+  forwardRef,
+  useMemo,
+  useState,
+  type ComponentPropsWithoutRef,
+  type ElementRef
+} from "react"
 
 import { cn } from "@lib/utils"
 
@@ -14,7 +20,10 @@ type SliderProps = ComponentPropsWithoutRef<typeof SliderPrimitive.Root> & {
 }
 
 const Slider = forwardRef<ElementRef<typeof SliderPrimitive.Root>, SliderProps>(
-  ({ onValueChange, formatTooltip, className, value, defaultValue, ...props }, ref) => {
+  (
+    { onValueChange, formatTooltip, className, value, defaultValue, min = 0, max = 100, ...props },
+    ref
+  ) => {
     const [internalValue, setInternalValue] = useState<number[]>(defaultValue || [0])
 
     const currentValue = value || internalValue
@@ -23,15 +32,24 @@ const Slider = forwardRef<ElementRef<typeof SliderPrimitive.Root>, SliderProps>(
 
     const formatValue = formatTooltip || ((value: number) => value.toString())
 
+    const _values = useMemo(
+      () =>
+        Array.isArray(value) ? value : Array.isArray(defaultValue) ? defaultValue : [min, max],
+      [value, defaultValue, min, max]
+    )
+
     return (
       <TooltipProvider delayDuration={0}>
         <SliderPrimitive.Root
           ref={ref}
           className={cn(
-            "group relative flex w-full touch-none select-none items-center",
+            "group relative flex w-full touch-none select-none items-center data-[orientation=vertical]:h-full data-[orientation=vertical]:min-h-44 data-[orientation=vertical]:w-auto data-[orientation=vertical]:flex-col data-[disabled]:opacity-50",
             className
           )}
           value={currentValue}
+          defaultValue={defaultValue}
+          min={min}
+          max={max}
           onValueChange={(newValue) => {
             setTooltipOpen(true)
             if (!value) {
@@ -41,22 +59,24 @@ const Slider = forwardRef<ElementRef<typeof SliderPrimitive.Root>, SliderProps>(
           }}
           {...props}
         >
-          <SliderPrimitive.Track className="relative h-1 w-full grow overflow-hidden rounded-full bg-primary/20 transition-colors">
-            <SliderPrimitive.Range className="absolute h-full bg-primary" />
+          <SliderPrimitive.Track className="relative h-1 w-full grow overflow-hidden rounded-full bg-primary/20 transition-colors data-[orientation=horizontal]:h-1 data-[orientation=vertical]:h-full data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-1">
+            <SliderPrimitive.Range className="absolute h-full bg-primary data-[orientation=horizontal]:h-full data-[orientation=vertical]:w-full" />
           </SliderPrimitive.Track>
-          <Tooltip open={tooltipOpen}>
-            <TooltipTrigger asChild>
-              <SliderPrimitive.Thumb
-                onBlur={() => setTooltipOpen(false)}
-                className="block h-3 w-3 rounded-full border border-primary/50 bg-primary opacity-0 transition-opacity ease-in-out hover:opacity-100 focus:opacity-100 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 group-hover:opacity-100"
-              />
-            </TooltipTrigger>
-            <TooltipContent>
-              <Typography variant="span" affects="small">
-                {formatValue(currentValue[0])}
-              </Typography>
-            </TooltipContent>
-          </Tooltip>
+          {Array.from({ length: _values.length }, (_, index) => (
+            <Tooltip key={index} open={tooltipOpen}>
+              <TooltipTrigger asChild>
+                <SliderPrimitive.Thumb
+                  onBlur={() => setTooltipOpen(false)}
+                  className="block h-3 w-3 rounded-full border border-primary/50 bg-primary opacity-0 transition-opacity ease-in-out hover:opacity-100 focus:opacity-100 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 group-hover:opacity-100"
+                />
+              </TooltipTrigger>
+              <TooltipContent>
+                <Typography variant="span" affects="small">
+                  {formatValue(currentValue[index] || currentValue[0])}
+                </Typography>
+              </TooltipContent>
+            </Tooltip>
+          ))}
         </SliderPrimitive.Root>
       </TooltipProvider>
     )
