bef39a6d3cb2d9e98c74b0944b103a7646e730b7
ItsLhuis <itslhuis.dev@gmail.com>
2025-11-28 18:10:41 +0000

    Update Text component with fixed lineHeight multiplier

--- apps/mobile/components/ui/Text.tsx
+++ apps/mobile/components/ui/Text.tsx
@@ -48,36 +48,42 @@ const textStyles = createStyleSheet(({ theme }) => ({
         h1: {
           fontSize: theme.fontSize("4xl"),
           fontFamily: "SpaceGrotesk-Bold",
-          letterSpacing: theme.letterSpacing("tight")
+          letterSpacing: theme.letterSpacing("tight"),
+          lineHeight: theme.fontSize("4xl") * 1.25
         },
         h2: {
           fontSize: theme.fontSize("3xl"),
           fontFamily: "SpaceGrotesk-Bold",
           letterSpacing: theme.letterSpacing("tight"),
-          paddingBottom: theme.space("sm")
+          paddingBottom: theme.space("sm"),
+          lineHeight: theme.fontSize("3xl") * 1.25
         },
         h3: {
           fontSize: theme.fontSize("2xl"),
           fontFamily: "SpaceGrotesk-Bold",
-          letterSpacing: theme.letterSpacing("tight")
+          letterSpacing: theme.letterSpacing("tight"),
+          lineHeight: theme.fontSize("2xl") * 1.25
         },
         h4: {
           fontSize: theme.fontSize("xl"),
           fontFamily: "SpaceGrotesk-Bold",
-          letterSpacing: theme.letterSpacing("tight")
+          letterSpacing: theme.letterSpacing("tight"),
+          lineHeight: theme.fontSize("xl") * 1.25
         },
         h5: {
           fontSize: theme.fontSize("lg"),
           fontFamily: "SpaceGrotesk-Bold",
-          letterSpacing: theme.letterSpacing("tight")
+          letterSpacing: theme.letterSpacing("tight"),
+          lineHeight: theme.fontSize("lg") * 1.25
         },
         h6: {
           fontSize: theme.fontSize("base"),
           fontFamily: "SpaceGrotesk-Bold",
-          letterSpacing: theme.letterSpacing("tight")
+          letterSpacing: theme.letterSpacing("tight"),
+          lineHeight: theme.fontSize("base") * 1.25
         },
         p: {
-          lineHeight: theme.lineHeight(7)
+          lineHeight: theme.fontSize() * 1.25
         },
         blockquote: {
           borderLeftWidth: theme.borderWidth(2),
@@ -108,28 +114,55 @@ const textStyles = createStyleSheet(({ theme }) => ({
       size: {
         xs: {
           fontSize: theme.fontSize("xs"),
-          lineHeight: theme.lineHeight("none")
+          lineHeight: theme.fontSize("xs") * 1.25
         },
         sm: {
-          fontSize: theme.fontSize("sm")
+          fontSize: theme.fontSize("sm"),
+          lineHeight: theme.fontSize("sm") * 1.25
         },
         base: {
-          fontSize: theme.fontSize()
+          fontSize: theme.fontSize(),
+          lineHeight: theme.fontSize() * 1.25
         },
         lg: {
-          fontSize: theme.fontSize("lg")
+          fontSize: theme.fontSize("lg"),
+          lineHeight: theme.fontSize("lg") * 1.25
         },
         xl: {
-          fontSize: theme.fontSize("xl")
+          fontSize: theme.fontSize("xl"),
+          lineHeight: theme.fontSize("xl") * 1.25
         },
         "2xl": {
-          fontSize: theme.fontSize("2xl")
+          fontSize: theme.fontSize("2xl"),
+          lineHeight: theme.fontSize("2xl") * 1.25
         },
         "3xl": {
-          fontSize: theme.fontSize("3xl")
+          fontSize: theme.fontSize("3xl"),
+          lineHeight: theme.fontSize("3xl") * 1.25
         },
         "4xl": {
-          fontSize: theme.fontSize("4xl")
+          fontSize: theme.fontSize("4xl"),
+          lineHeight: theme.fontSize("4xl") * 1.25
+        },
+        "5xl": {
+          fontSize: theme.fontSize("5xl"),
+          lineHeight: theme.fontSize("5xl") * 1.25
+        },
+        "6xl": {
+          fontSize: theme.fontSize("6xl"),
+          lineHeight: theme.fontSize("6xl") * 1.25
+        },
+        "7xl": {
+          fontSize: theme.fontSize("7xl"),
+          lineHeight: theme.fontSize("7xl") * 1.25
+        },
+        "8xl": {
+          fontSize: theme.fontSize("8xl"),
+          lineHeight: theme.fontSize("8xl") * 1.25
+        },
+        "9xl": {
+          fontSize: theme.fontSize("9xl"),
+          lineHeight: theme.fontSize("9xl") * 1.25
         }
       },
       weight: {
