1303bcd73f83d2de581c647fc92ee05be7043f5a
ItsLhuis <itslhuis.dev@gmail.com>
2025-12-07 16:52:14 +0000

    refactor(desktop): simplify HomeHeader layout
    - Remove unnecessary div wrapper and adjust padding in HomeHeader for a cleaner layout.

--- apps/desktop/src/features/home/components/HomeHeader.tsx
+++ apps/desktop/src/features/home/components/HomeHeader.tsx
@@ -6,15 +6,13 @@ const HomeHeader = () => {
   const { t } = useTranslation()
 
   return (
-    <Header className="flex flex-col gap-6 pb-9">
-      <div className="flex flex-1 items-end gap-6">
-        <Typography
-          variant="h1"
-          className="line-clamp-2 break-all text-4xl md:text-6xl lg:text-7xl xl:text-8xl"
-        >
-          {t("home.title")}
-        </Typography>
-      </div>
+    <Header className="flex flex-col gap-6 pb-3">
+      <Typography
+        variant="h1"
+        className="line-clamp-2 text-4xl break-all md:text-6xl lg:text-7xl xl:text-8xl"
+      >
+        {t("home.title")}
+      </Typography>
     </Header>
   )
 }
