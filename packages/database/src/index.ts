e0969480a948947af238e827279afcf6a4d0e97b
ItsLhuis <itslhuis.dev@gmail.com>
2025-12-22 22:31:06 +0000

    refactor(database): simplify type exports using wildcard re-export

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,12 +1,3 @@
 export * from "./helpers"
 export * as schema from "./schema"
-export type {
-  Song,
-  InsertSong,
-  Artist,
-  InsertArtist,
-  Album,
-  InsertAlbum,
-  Playlist,
-  InsertPlaylist
-} from "./types"
+export type * from "./types"
