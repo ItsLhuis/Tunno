10242414c5010c0a816d5348748cfe2601e65d6b
ItsLhuis <itslhuis.dev@gmail.com>
2026-01-16 00:52:25 +0000

    feat(mobile): configure android widget entry point and expo plugin

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -1,11 +1,37 @@
 import { ConfigContext, ExpoConfig } from "@expo/config"
 
+import { type WithAndroidWidgetsParams } from "react-native-android-widget"
+
+const widgetConfig: WithAndroidWidgetsParams = {
+  fonts: [
+    "./assets/fonts/SpaceGrotesk-Light.ttf",
+    "./assets/fonts/SpaceGrotesk-Regular.ttf",
+    "./assets/fonts/SpaceGrotesk-Medium.ttf",
+    "./assets/fonts/SpaceGrotesk-Bold.ttf"
+  ],
+  widgets: [
+    {
+      name: "MiniPlayer",
+      label: "Tunno Player",
+      description: "Control your music from the home screen",
+      previewImage: "./assets/images/app/icon.png",
+      minWidth: "140dp",
+      minHeight: "70dp",
+      targetCellWidth: 2,
+      targetCellHeight: 2,
+      maxResizeWidth: "400dp",
+      maxResizeHeight: "180dp",
+      resizeMode: "horizontal|vertical",
+      updatePeriodMillis: 1800000
+    }
+  ]
+}
+
 module.exports = ({ config }: ConfigContext): Partial<ExpoConfig> => {
   const plugins = config.plugins ?? []
-
   return {
     ...config,
-    plugins: [...plugins],
+    plugins: [...plugins, ["react-native-android-widget", widgetConfig]],
     extra: {
       ...config.extra,
       env: {}
--- apps/mobile/features/player/components/index.ts
+++ apps/mobile/features/player/components/index.ts
@@ -1,2 +1,3 @@
 export * from "./BottomPlayer"
 export * from "./Player"
+export * from "./Widget"
