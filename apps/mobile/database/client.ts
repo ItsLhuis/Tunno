c0e4f2b768b33aa4c5bdb51ad61183ea45407862
ItsLhuis <itslhuis.dev@gmail.com>
2026-01-26 17:54:20 +0000

    perf(mobile): add sqlite pragmas for database performance

--- apps/mobile/database/client.ts
+++ apps/mobile/database/client.ts
@@ -7,6 +7,9 @@ const databaseName = "database.db"
 
 const expoDatabase = openDatabaseSync(databaseName, { enableChangeListener: true })
 expoDatabase.execSync("PRAGMA foreign_keys = ON;")
+expoDatabase.execSync("PRAGMA journal_mode = WAL")
+expoDatabase.execSync("PRAGMA synchronous = NORMAL")
+expoDatabase.execSync("PRAGMA temp_store = MEMORY")
 
 const database = drizzle(expoDatabase, { schema })
 
